ef1bfd5f741c8f3b36b189e565ce51338f7f698f
Rama Diaz <ramadiaz221@gmail.com>
2024-01-15 00:01:18 +0700

    Fix error

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,8 +2,7 @@ import Navbar from "@/components/Utilites/Navbar";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Utilites/Footer";
-import { Analytics } from '@vercel/analytics'
-
+import { Analytics } from "@vercel/analytics";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,9 +16,10 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
-        <div className="min-h-screen bg-zinc-950">{children}</div>
-        <Footer/>
-        <Analytics/>
+        <div className="min-h-screen bg-zinc-950">
+          {children} <Analytics />
+        </div>
+        <Footer />
       </body>
     </html>
   );
